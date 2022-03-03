3524a39eb765e2163c82a0f4189681dd6f79d2b4
heyp999 <heyp999@gmail.com>
2022-03-03 11:40:20 -0800

    Add Advantage Shopping Cart app scripts

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -10,12 +10,19 @@ class ShoppingCartAppPositiveTestCases(unittest.TestCase):
     @staticmethod
     def test_advantage_shopping_cart_case2():
         methods.setup()
+        sleep(1)
         methods.check_homepage()
+        sleep(1)
         methods.register_new_user()
+        sleep(1)
         methods.logout()
+        sleep(1)
         methods.login(locators.new_username, locators.new_password)
+        sleep(1)
         methods.del_current_user()
+        sleep(1)
         methods.check_user_account_deleted(locators.new_username, locators.new_password)
+        sleep(1)
         methods.teardown()
 
 
